15a3be052dd142db685dc20b7666ee18472ac77a
Aaron Kawalsky <aaron@clued.xyz>
2024-12-27 16:49:08 -0500

    use stork configs

--- cdk/gasolina/config/index.ts
+++ cdk/gasolina/config/index.ts
@@ -10,16 +10,26 @@ export const CONFIG: {
         extraContextRequestUrl?: string
     }
 } = {
-    // EDIT: aws account number
-    '<aws-account-number>': {
+    '545009847164': {
         gasolinaRepo: 'us-east1-docker.pkg.dev/lz-docker/gasolina/gasolina',
         appVersion: 'latest', // EDIT: version and tag of the gasolina image
-        projectName: '<project_name>', // EDIT: project_name e.g. foobar-gasolina
-        environment: '<environment>', // EDIT: environment e.g. mainnet/testnet
+        projectName: 'stork-external-dev-gasolina', // EDIT: project_name e.g. foobar-gasolina
+        environment: 'testnet', // EDIT: environment e.g. mainnet/testnet
         availableChainNames:
             'ethereum,bsc,avalanche,polygon,arbitrum,optimism,fantom', // EDIT: all the chains gasolina will support that matches those listed in providers
-        signerType: 'MNEMONIC', // EDIT: MNEMONIC or KMS
-        // kmsNumOfSigners: 1, // EDIT: only required if signerType is KMS
+        signerType: 'KMS', // EDIT: MNEMONIC or KMS
+        kmsNumOfSigners: 1, // EDIT: only required if signerType is KMS
+        // extraContextRequestUrl: undefined // EDIT: optional
+    },
+    '195275642154': {
+        gasolinaRepo: 'us-east1-docker.pkg.dev/lz-docker/gasolina/gasolina',
+        appVersion: 'latest', // EDIT: version and tag of the gasolina image
+        projectName: 'stork-external-prod-gasolina', // EDIT: project_name e.g. foobar-gasolina
+        environment: 'mainnet', // EDIT: environment e.g. mainnet/testnet
+        availableChainNames:
+            'ethereum,bsc,avalanche,polygon,arbitrum,optimism,fantom', // EDIT: all the chains gasolina will support that matches those listed in providers
+        signerType: 'KMS', // EDIT: MNEMONIC or KMS
+        kmsNumOfSigners: 1, // EDIT: only required if signerType is KMS
         // extraContextRequestUrl: undefined // EDIT: optional
     },
 }
